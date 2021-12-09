da22606bfabcce223184df1f999e31f97b4d8ccc
Kazi Asad <kaziasad061@gmail.com>
2021-12-10 02:20:00 +0600

    commit to maoin

--- base/views.py
+++ base/views.py
@@ -24,3 +24,7 @@ def room(request,pk):
 #             room=i
     context = {'room':room}
     return render(request,'base/room.html',context)
+
+def createRoom(request):
+    context ={}
+    return render(request, 'base/room_form.html',context)
